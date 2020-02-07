0084173f22dc934f89c62a74c63644ce879c38e7
LucieO <luciecamilleory@gmail.com>
2020-02-07 16:52:15 +0100

    Updates

---
+++ precache-manifest.48fcf44116ffc76ed47d7ea182b2ad6f.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "691663212015e51dd037b8f071e205f7",
+    "url": "/Like-a-jedi/index.html"
+  },
+  {
+    "revision": "a3dccacec8d78273a580",
+    "url": "/Like-a-jedi/static/css/main.aa44f140.chunk.css"
+  },
+  {
+    "revision": "3e86cbdaf55aaf42236a",
+    "url": "/Like-a-jedi/static/js/2.b26ddc99.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/Like-a-jedi/static/js/2.b26ddc99.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a3dccacec8d78273a580",
+    "url": "/Like-a-jedi/static/js/main.424eef84.chunk.js"
+  },
+  {
+    "revision": "e6ebc1397e1891fa8b38",
+    "url": "/Like-a-jedi/static/js/runtime-main.81743366.js"
+  },
+  {
+    "revision": "32a0431cb71f1e33255c14e6e29c4523",
+    "url": "/Like-a-jedi/static/media/Stars.32a0431c.mp4"
+  },
+  {
+    "revision": "36fb2e91711426a9ab5221e54329e28e",
+    "url": "/Like-a-jedi/static/media/awing.36fb2e91.png"
+  },
+  {
+    "revision": "d43df86b70c330fa7188c6aaaebc816a",
+    "url": "/Like-a-jedi/static/media/battledroid.d43df86b.svg"
+  },
+  {
+    "revision": "5bd4c88c7e8c67bdc9adfe2a7fa5d268",
+    "url": "/Like-a-jedi/static/media/blasterSolo.5bd4c88c.svg"
+  },
+  {
+    "revision": "66fa43cbc5b7382cf03f24c5fee4b339",
+    "url": "/Like-a-jedi/static/media/boonda.66fa43cb.jpeg"
+  },
+  {
+    "revision": "b240d715437d1942404b8d37f2734312",
+    "url": "/Like-a-jedi/static/media/c3po.b240d715.svg"
+  },
+  {
+    "revision": "a328a80f749a3a343aab69943ca5b8cb",
+    "url": "/Like-a-jedi/static/media/car.a328a80f.svg"
+  },
+  {
+    "revision": "cf7fed8d20264b8bb27e07dde91106a7",
+    "url": "/Like-a-jedi/static/media/cup.cf7fed8d.svg"
+  },
+  {
+    "revision": "05c864a3ac004f3a8207673aec53cfd2",
+    "url": "/Like-a-jedi/static/media/darthvader.05c864a3.svg"
+  },
+  {
+    "revision": "47728000f8bfb95ece7ed8b844f35e28",
+    "url": "/Like-a-jedi/static/media/death.47728000.svg"
+  },
+  {
+    "revision": "bf01f41ba05152ab123446b73e288ecf",
+    "url": "/Like-a-jedi/static/media/deathstar.bf01f41b.png"
+  },
+  {
+    "revision": "a38a5f19e7519b4b0c8b128750e17f78",
+    "url": "/Like-a-jedi/static/media/delrakkin.a38a5f19.jpg"
+  },
+  {
+    "revision": "d0d584963e35db2713039006b20791c7",
+    "url": "/Like-a-jedi/static/media/dvArmor.d0d58496.svg"
+  },
+  {
+    "revision": "4b97031545ebfdcfc24d513bf73ef011",
+    "url": "/Like-a-jedi/static/media/falcon.4b970315.png"
+  },
+  {
+    "revision": "ceeb2447341b3461a16f3cad87b19e7f",
+    "url": "/Like-a-jedi/static/media/falcon.ceeb2447.svg"
+  },
+  {
+    "revision": "2b1dcb72e89793630a39df1cc4a89c72",
+    "url": "/Like-a-jedi/static/media/female.2b1dcb72.jpg"
+  },
+  {
+    "revision": "94c44c66ca7febcbb10e9c32531d14c5",
+    "url": "/Like-a-jedi/static/media/flea.94c44c66.svg"
+  },
+  {
+    "revision": "e1e4a812a387897be4d242808259a9fa",
+    "url": "/Like-a-jedi/static/media/galactic-empire.e1e4a812.svg"
+  },
+  {
+    "revision": "baffb0082b0d2feb2ee310784b70e01a",
+    "url": "/Like-a-jedi/static/media/injury.baffb008.svg"
+  },
+  {
+    "revision": "5ebeb5142cd3bedf9aacaeaa5164ff05",
+    "url": "/Like-a-jedi/static/media/jeans.5ebeb514.svg"
+  },
+  {
+    "revision": "6cefb9f860c5a5fef4e4490e2d8ee53d",
+    "url": "/Like-a-jedi/static/media/lasersword.6cefb9f8.svg"
+  },
+  {
+    "revision": "7104a07b995fd4496f0046a5e10c695d",
+    "url": "/Like-a-jedi/static/media/lasersword2.7104a07b.svg"
+  },
+  {
+    "revision": "b5cb91db438d3ec5c27d7fc7f8fc7bde",
+    "url": "/Like-a-jedi/static/media/luck.b5cb91db.svg"
+  },
+  {
+    "revision": "29dd74975d21437a76b13064beb1b651",
+    "url": "/Like-a-jedi/static/media/male.29dd7497.jpg"
+  },
+  {
+    "revision": "16afa726f96cb582ff9b758ef4d1c15c",
+    "url": "/Like-a-jedi/static/media/porg.16afa726.png"
+  },
+  {
+    "revision": "2c3452e079e36dcfea8858d6d8f58a00",
+    "url": "/Like-a-jedi/static/media/porg.2c3452e0.svg"
+  },
+  {
+    "revision": "b1d92501b384a987da7c321188c865d8",
+    "url": "/Like-a-jedi/static/media/r2d2.b1d92501.svg"
+  },
+  {
+    "revision": "8fffe77520840b8907187b105eee5f1e",
+    "url": "/Like-a-jedi/static/media/rebel-alliance.8fffe775.svg"
+  },
+  {
+    "revision": "5dc2c39ca1bc73df045410027367d15b",
+    "url": "/Like-a-jedi/static/media/spacebg.5dc2c39c.jpg"
+  },
+  {
+    "revision": "b1bf40d334fa70bef3905a70f745aeb8",
+    "url": "/Like-a-jedi/static/media/spear.b1bf40d3.svg"
+  },
+  {
+    "revision": "1bb952ef93aa2245c72431de2aec3bcc",
+    "url": "/Like-a-jedi/static/media/starwarslogo.1bb952ef.png"
+  },
+  {
+    "revision": "d7187243227f8c9492694866aa4b9b2d",
+    "url": "/Like-a-jedi/static/media/stblaster.d7187243.svg"
+  },
+  {
+    "revision": "b9b85785b80c0e37502c9f81dca197ba",
+    "url": "/Like-a-jedi/static/media/stick.b9b85785.svg"
+  },
+  {
+    "revision": "881902d41c12c82c8fb1dad0bb672578",
+    "url": "/Like-a-jedi/static/media/stormtrooper.881902d4.jpg"
+  },
+  {
+    "revision": "de6447bbd2cba39165f8eff9d0e123d0",
+    "url": "/Like-a-jedi/static/media/stormtrooper.de6447bb.svg"
+  },
+  {
+    "revision": "418a12bed4a4e478000d8e8e07e74720",
+    "url": "/Like-a-jedi/static/media/tattoine.418a12be.jpg"
+  },
+  {
+    "revision": "5ffac1b5789179624193c40ae8e8270d",
+    "url": "/Like-a-jedi/static/media/thyferra.5ffac1b5.jpeg"
+  },
+  {
+    "revision": "59950944a21c130be7cbb4485896c3d0",
+    "url": "/Like-a-jedi/static/media/tiefighter.59950944.png"
+  },
+  {
+    "revision": "a670c32efddfc51837a03cac4d933568",
+    "url": "/Like-a-jedi/static/media/togoria.a670c32e.jpg"
+  },
+  {
+    "revision": "53ca63ec413a491a45f35161a1983cfb",
+    "url": "/Like-a-jedi/static/media/vactooine.53ca63ec.jpg"
+  },
+  {
+    "revision": "6354c5614083035656107c2096580a5b",
+    "url": "/Like-a-jedi/static/media/xwing.6354c561.png"
+  }
+]);
